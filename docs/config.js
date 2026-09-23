a555f1aeaa51d09785b95859a0bc62b8214e228b
SalvadorGPIktan <salvador.gaytan@inegi.org.mx>
2026-09-23 12:53:23 -0600

    Update Supabase URL in config.js

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 window.QUINIELA_CONFIG = {
-  supabaseUrl: '',
+  supabaseUrl: 'https://llihvdpqpwdodqchhtts.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsaWh2ZHBxcHdkb2RxY2hodHRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAxODE2NjQsImV4cCI6MjEwNTc1NzY2NH0.xMIFtZzNnmo_aS4xOVn8ydQu59xoVekaW__HwXD9rxk', // Clave pública anon; NUNCA service_role.
   title: 'La Banda',
 };
